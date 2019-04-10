e21a16c7b5354cd4ddd540243a2d9cf6de033347
okunishinishi <okunishinishi@gmail.com>
2019-04-10 14:59:33 +0900

    Fix deep scan warnings

--- packages/demo-ui/lib/TheDemoComponentStyle.jsx
+++ packages/demo-ui/lib/TheDemoComponentStyle.jsx
@@ -8,7 +8,6 @@
 import c from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { ThemeValues } from '@the-/const-ui'
 import { TheStyle } from '@the-/ui-style'
 import { asStyleData } from '@the-/util-ui'
 
