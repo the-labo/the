164d6a0c59f10cc97e1839fafe362196a7cdf3b6
okunishinishi <okunishinishi@gmail.com>
2019-05-10 17:17:26 +0900

    Update map

--- packages/ui-map/doc/api/api.md
+++ packages/ui-map/doc/api/api.md
@@ -33,7 +33,7 @@
 ## @the-/ui-map
 Geo map for the-components
 
-**Version**: 15.4.4  
+**Version**: 15.4.5  
 <a name="TheMap"></a>
 
 ## TheMap
--- packages/ui-map/lib/index.jsx
+++ packages/ui-map/lib/index.jsx
@@ -2,7 +2,7 @@
 /**
  * @module @the-/ui-map
  * @typicalname uiMap
- * @version 15.4.4
+ * @version 15.4.5
  * @description Geo map for the-components
  */
 'use strict'
