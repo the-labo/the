d9ee9ceaedc8832d269313463684b26cd7a2a053
okunishinishi <okunishinishi@gmail.com>
2019-10-17 20:58:34 +0900

    uPdate deps

--- packages/ui-caught/doc/api/api.md
+++ packages/ui-caught/doc/api/api.md
@@ -21,7 +21,7 @@
 ## @the-/ui-caught
 Component to show caught errors
 
-**Version**: 15.4.10  
+**Version**: 15.4.12  
 
 * [@the-/ui-caught](#module_@the-/ui-caught)
     * [.TheCaught](#module_@the-/ui-caught.TheCaught)
--- packages/ui-caught/lib/TheCaught.jsx
+++ packages/ui-caught/lib/TheCaught.jsx
@@ -12,45 +12,40 @@ import { eventHandlersFor, htmlAttributesFor } from '@the-/util-ui'
  * @memberof module:@the-/ui-caught
  * @class TheCaught
  */
-class TheCaught extends React.Component {
-  render() {
-    const {
-      props,
-      props: { children, className, error, info },
-    } = this
+const TheCaught = (props) => {
+  const { children, className, error, info } = props
 
-    const { componentStack } = info || {}
-    if (!error) {
-      return null
-    }
-
-    return (
-      <div
-        {...htmlAttributesFor(props, { except: ['className'] })}
-        {...eventHandlersFor(props, { except: [] })}
-        className={c('the-caught', className)}
-      >
-        <h3 className='the-caught-title' role='heading'>
-          {String(error.message || error)}
-        </h3>
-        {children}
-        <TheCondition if={!isProduction()}>
-          <pre className='the-caught-stack'>
-            <div>{error.stack}</div>
-            <hr className='the-caught-line' />
-            <TheCondition if={!!componentStack}>
-              <div>{componentStack}</div>
-            </TheCondition>
-          </pre>
-        </TheCondition>
-      </div>
-    )
+  const { componentStack } = info || {}
+  if (!error) {
+    return null
   }
+
+  return (
+    <div
+      {...htmlAttributesFor(props, { except: ['className'] })}
+      {...eventHandlersFor(props, { except: [] })}
+      className={c('the-caught', className)}
+    >
+      <h3 className='the-caught-title' role='heading'>
+        {String(error.message || error)}
+      </h3>
+      {children}
+      <TheCondition if={!isProduction()}>
+        <pre className='the-caught-stack'>
+          <div>{error.stack}</div>
+          <hr className='the-caught-line' />
+          <TheCondition if={!!componentStack}>
+            <div>{componentStack}</div>
+          </TheCondition>
+        </pre>
+      </TheCondition>
+    </div>
+  )
 }
 
 TheCaught.propTypes = {
   /** Caught error */
-  error: PropTypes.string,
+  error: PropTypes.oneOf([PropTypes.object, PropTypes.string]),
   /** Error Info */
   info: PropTypes.object,
 }
