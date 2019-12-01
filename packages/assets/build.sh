64f7d65e5aff4c9193bf13c0b5d7d7dccc115e5f
okunishinishi <okunishinishi@gmail.com>
2019-12-01 09:41:42 +0900

    add minified css

---
+++ packages/assets/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+
+npx uglifycss assets/normalize/css/normalize.css --output assets/normalize/css/normalize.min.css
+npx uglifycss assets/leaflet/css/leaflet.css --output assets/leaflet/css/leaflet.min.css
+
+
+
