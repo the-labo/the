3916ad6f7621afb8c7b025d5fea9be19a789f3ba
Taka Okunishi <okunishinishi@gmail.com>
2020-12-02 14:00:07 +0900

    publish ui-date, close #61

--- packages/ui-date/doc/api/api.md
+++ packages/ui-date/doc/api/api.md
@@ -26,7 +26,7 @@
 ## @the-/ui-date
 Date ui
 
-**Version**: 17.0.2  
+**Version**: 17.0.3  
 <a name="TheDateStyle"></a>
 
 ## TheDateStyle()
--- packages/ui-date/lib/index.jsx
+++ packages/ui-date/lib/index.jsx
@@ -4,7 +4,7 @@
  * @description Date ui
  * @module @the-/ui-date
  * @typicalname uiDate
- * @version 17.0.2
+ * @version 17.0.3
  */
 'use strict'
 
